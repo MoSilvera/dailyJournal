0e0f098c46b054a3f6c5d030ed23d19e86deec42
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-01-21 14:38:16 -0600

    Adding basic info

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,14 +1,5 @@
-let allJournalEntries = []
-
-const objectJournalEntry = {
-    date: "01/16/19",
-    conceptsCovered: "Publishing components to the DOM",
-    journalContents: "After getting the opportunity to explain this like 12 times, I really feel like I have a handle on it.",
-    mood: "curious", 
-}
-
-allJournalEntries.push(objectJournalEntry)
-
-console.log(allJournalEntries)
-
-const makeJournal
+fetch() // Fetch from the API
+    .then()  // Parse as JSON
+    .then(entries => {
+        // What should happen when we finally have the array?
+    })
